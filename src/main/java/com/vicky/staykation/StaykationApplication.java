2bba5ac4642eb59325b742d50a82975b921e9b84
Vicky Shetty <vickyshetty@Vickys-Mac-mini.local>
2026-04-05 09:47:21 +0530

    initial: Spring Boot project setup

---
+++ src/main/java/com/vicky/staykation/StaykationApplication.java
@@ -0,0 +1,13 @@
+package com.vicky.staykation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaykationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaykationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vicky/staykation/StaykationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vicky.staykation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaykationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
